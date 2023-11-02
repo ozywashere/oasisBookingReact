c8fecadb02396cde0d6b8957e47eead441ac31b0
ozywashere <durduarslantas@ogr.iu.edu.tr>
2023-11-02 03:38:55 +0300

    supabase key added as a string

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://xehcqozaniusnvseedui.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlaGNxb3phbml1c252c2VlZHVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMxMzQ0NTMsImV4cCI6MjAwODcxMDQ1M30.uE75EFyYowMA9eaLlNo1IyTUNhuYU6DqSitVd_ij61A';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
